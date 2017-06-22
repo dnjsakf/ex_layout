4637ad7b69bc729cf6c2a5e658ce3ac5204e896c
heoDolf <dnjsakf@gmail.com>
2017-06-22 17:22:00 +0900

    get -> post

--- public/js/test.js
+++ public/js/test.js
@@ -8,13 +8,13 @@ $(document).ready(function(){
     val: textarea.val()
   });
   var language = 'java'
-  $.get('./init/'+language+'.txt', function(data){
+  $.post('init/'+language+'.txt', function(data){
     editor.setValue(data);
   });
   $(".select_compiler").change(function(){
     var language = $(".select_compiler option:selected").val();
     console.log(language);
-    $.get('./init/'+language+'.txt', function(data){
+    $.post('init/'+language+'.txt', function(data){
       editor.setValue(data);
     });
   });
